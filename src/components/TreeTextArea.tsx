68c0768c289f496bab34ce1bf5a144df25b00bec
Toci <tocirml2@gmail.com>
2021-05-28 14:29:32 -0600

    Add funtionality to update tree with textarea

--- src/components/TreeTextArea.tsx
+++ src/components/TreeTextArea.tsx
@@ -9,6 +9,26 @@ export interface TreeTextAreaProps {
 
 export const TreeTextArea: React.FunctionComponent<TreeTextAreaProps> =
   props => {
+    const jsonTree = JSON.stringify(props.treeNode, null, 2);
+    const [text, setText] = React.useState<string>(jsonTree);
+    const [error, setError] = React.useState<string | null>(null);
+
+    React.useEffect(() => {
+      setText(jsonTree);
+    }, [jsonTree]);
+
+    const changeHandler = (event: any) => {
+      const { value } = event.target;
+      setText(value);
+      try {
+        let validJson = JSON.parse(value);
+        setError(null);
+        props.onChange(validJson);
+      } catch {
+        setError('Invalid JSON format');
+      }
+    };
+
     return (
       <div className="tree-text-area">
         <label htmlFor="tree-text">Tree Text:</label>
@@ -17,8 +37,10 @@ export const TreeTextArea: React.FunctionComponent<TreeTextAreaProps> =
           name="tree-text"
           cols={100}
           rows={20}
-          value={JSON.stringify(props.treeNode, null, 2)}
+          value={text}
+          onChange={changeHandler}
         ></textarea>
+        {error && <div className="json-error">{error}</div>}
       </div>
     );
   };
